1512255980fbb5df1d2b43874003e5c0859a90af
SteveStudentUsc <steve.pinto00@usc.edu.co>
2025-05-22 17:48:50 -0500

    se agrego secciones nuevas para funciones de especialidades y disponibilidad

--- src/main/java/co/edu/usc/TeacherGo/model/Solicitudes.java
+++ src/main/java/co/edu/usc/TeacherGo/model/Solicitudes.java
@@ -3,24 +3,27 @@ package co.edu.usc.TeacherGo.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Solicitudes {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private Usuarios estudiante_id;
+    private String fechaHora;
+
+    private String estado; // 'Pendiente', 'Aceptada', 'Rechazada'
 
     @ManyToOne
-    private Docentes docente_id;
+    @JoinColumn(name = "estudiante_id")
+    private Usuarios estudiante;
 
-    private String fecha_hora;
-
-    private String estado;
-
+    @ManyToOne
+    @JoinColumn(name = "docente_id")
+    private Docentes docente;
 }
